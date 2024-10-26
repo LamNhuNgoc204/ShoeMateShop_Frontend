f2a8b26902b8b8cbfd566df6f070b85ad8c59a12
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-10-26 21:11:30 +0700

    Header notify

--- src/screens/Notifycation/index.js
+++ src/screens/Notifycation/index.js
@@ -1,10 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Header from '../../components/Header';
 
 const Notifycation = () => {
   return (
     <View>
-      <Text>Notifycation</Text>
+      <Header name={'Thông báo'} />
     </View>
   );
 };
