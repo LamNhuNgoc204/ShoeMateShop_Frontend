bd3669beed373c5dbe1878ccc1c9b1dd8b5b19e1
DucDuy2104 <117710024+DucDuy2104@users.noreply.github.com>
2024-11-12 16:04:44 +0700

    push notification

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'intl-pluralrules';
-import {KeyboardAvoidingView} from 'react-native';
+import {KeyboardAvoidingView, PermissionsAndroid} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import appst from './src/constants/AppStyle';
@@ -10,6 +10,7 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './src/redux/store/store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging, { firebase } from '@react-native-firebase/messaging';
 const App = () => {
   useEffect(() => {
     const getLanguage = async () => {
@@ -21,6 +22,42 @@ const App = () => {
       }
     };
     getLanguage();
+  }, []);
+
+  async function checkPermission() {
+    try {
+      const granted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+      if (granted) {
+        console.log('Quyền đã được cấp.');
+        getFCMtoken();
+      } else {
+        console.log('Quyền chưa được cấp.');
+        const requestResult = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+        if (requestResult === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Quyền đã được cấp sau khi yêu cầu.');
+          getFCMtoken();
+        } else {
+          console.log('Quyền bị từ chối.');
+        }
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
+
+  const getFCMtoken = async () => {
+    try {
+      console.log('get token..')
+      const fcmToken = await messaging().getToken();
+      console.log('FCM token: ', fcmToken);
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  }
+
+  useEffect(() => {
+    checkPermission()
   }, []);
 
   return (
