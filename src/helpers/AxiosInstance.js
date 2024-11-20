79019be30a877a1dda15f724dba88a40912a7f36
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-11-21 01:03:05 +0700

    refresh token

--- src/helpers/AxiosInstance.js
+++ src/helpers/AxiosInstance.js
@@ -1,14 +1,35 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import dayjs from 'dayjs';
 
 const AxiosInstance = (contentType = 'application/json') => {
   const axiosInstance = axios.create({
     baseURL: `https://shoe-mate-shop-backend.vercel.app/`,
   });
-
   axiosInstance.interceptors.request.use(
     async config => {
-      const token = await AsyncStorage.getItem('token');
+      const expiredTokenDate = await AsyncStorage.getItem('expiredTokenDate');
+      let token = await AsyncStorage.getItem('token');
+
+      if (dayjs().isAfter(expiredTokenDate)) {
+        const response = await axios.post(
+          'https://shoe-mate-shop-backend.vercel.app/auth/refresh-token',
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        newestToken = response?.data?.data;
+        await AsyncStorage.setItem('token', newestToken);
+        await AsyncStorage.setItem(
+          'expiredTokenDate',
+          dayjs().add('7', 'day').format('YYYY-MM-DD HH:mm:ss'),
+        );
+      } else {
+        token = await AsyncStorage.getItem('token');
+      }
       // const token = '';
       config.headers = {
         Authorization: `Bearer ${token}`,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNav from './StackNav';
+import linking from '../../linking';
 
 const MainNav = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <StackNav />
     </NavigationContainer>
   );
