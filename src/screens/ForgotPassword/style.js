65b2ffaa669e5470dc5127186abc5b9fb67625ee
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-11-21 19:16:32 +0700

    Update UI

--- src/screens/ForgotPassword/style.js
+++ src/screens/ForgotPassword/style.js
@@ -1,62 +1,57 @@
-import { StyleSheet } from 'react-native'
-import appst from '../../constants/AppStyle';
+import {StyleSheet} from 'react-native';
 import {sizes, spacing} from '../../constants';
 import {fonts} from '../../constants/fonts';
 import {colors} from '../../constants/colors';
 
-
 export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: spacing.lg,
-        backgroundColor: 'white',
-      },
-      text1: {
-        fontSize: sizes.size32,
-        fontFamily: fonts.rlw_semibold,
-        color: colors.text_black,
-        marginTop: spacing.lg,
-        marginBottom: spacing.sm,
-        fontWeight: 'bold',
-      },
-      text2: {
-        fontSize: sizes.size18,
-        fontFamily: fonts.rlw_medium,
-        color: colors.color707B81,
-        marginBottom: spacing.sm,
-        textAlign: 'center',
-      },
-    
-     
-      
-      button: {
-        backgroundColor: colors.primary,
-        borderRadius: sizes.size14,
-        height: sizes.size50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: spacing.xl,
-      },
-      text5: {
-        fontSize: sizes.size16,
-        fontFamily: fonts.rlw_medium,
-        color: colors.colorFFF,
-        fontWeight: 'semibold',
-      },
-      press: {
-        width: '100%',
-        alignItems: 'center',
-        borderRadius: 12,
-        backgroundColor: colors.primary,
-        marginTop: spacing.xl,
-      },
-      textPress: {
-        color: colors.background_primary,
-        textAlign: 'center',
-        fontSize: sizes.size16,
-        fontFamily: fonts.rlw_bold,
-        paddingVertical: spacing.md,
-      },
-      
-})
+  container: {
+    flex: 1,
+    padding: spacing.lg,
+    backgroundColor: 'white',
+  },
+  text1: {
+    fontSize: sizes.size32,
+    fontFamily: fonts.rlw_semibold,
+    color: colors.text_black,
+    marginTop: spacing.lg,
+    marginBottom: spacing.sm,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text2: {
+    fontSize: sizes.size18,
+    fontFamily: fonts.rlw_medium,
+    color: colors.color707B81,
+    marginBottom: spacing.sm,
+    textAlign: 'center',
+  },
 
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: sizes.size14,
+    height: sizes.size50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: spacing.xl,
+  },
+  text5: {
+    fontSize: sizes.size16,
+    fontFamily: fonts.rlw_medium,
+    color: colors.colorFFF,
+    fontWeight: 'semibold',
+  },
+  press: {
+    width: '100%',
+    alignItems: 'center',
+    borderRadius: 12,
+    backgroundColor: colors.primary,
+    marginTop: 10,
+  },
+  textPress: {
+    color: colors.background_primary,
+    textAlign: 'center',
+    fontSize: sizes.size16,
+    fontFamily: fonts.rlw_bold,
+    paddingVertical: spacing.md,
+  },
+});
--- src/screens/OnBoardScreen/style.js
+++ src/screens/OnBoardScreen/style.js
@@ -1,50 +1,48 @@
-import { Dimensions, StyleSheet } from "react-native";
-import { fonts } from "../../constants/fonts";
-import { colors } from "../../constants/colors";
+import {Dimensions, StyleSheet} from 'react-native';
+import {fonts} from '../../constants/fonts';
+import {colors} from '../../constants/colors';
 
-
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const onBoardStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.background_onboard
-    },
-    onboardBg: {
-        width,
-        height,
-        resizeMode: 'cover'
-    },
-
-    contentContainer: {
-        alignItems: 'center',
-        position:'absolute',
-        left: 20,
-        right: 20,
-        bottom: 20
-    },
-    indicator: {
-        width: 118,
-        height: 5,
-        resizeMode: 'contain',
-        marginBottom: 60
-    },
-    title: {
-        fontFamily: fonts.rlw_bold,
-        fontSize:  34,
-        color: colors.white_EC,
-        lineHeight: 44,
-        marginBottom:  12,
-        textAlign: 'center'
-    },
-    content: {
-        fontFamily: fonts.pp_regular,
-        fontSize: 16,
-        lineHeight: 24,
-        color: colors.white_D8,
-        marginBottom:  30,
-        textAlign: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: colors.background_onboard,
+  },
+  onboardBg: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  contentContainer: {
+    alignItems: 'center',
+    position: 'absolute',
+    left: 20,
+    right: 20,
+    bottom: 20,
+  },
+  indicator: {
+    width: 118,
+    height: 5,
+    resizeMode: 'contain',
+    marginBottom: 60,
+  },
+  title: {
+    fontFamily: fonts.rlw_bold,
+    fontSize: 34,
+    color: colors.white_EC,
+    lineHeight: 44,
+    marginBottom: 12,
+    textAlign: 'center',
+  },
+  content: {
+    fontFamily: fonts.pp_regular,
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.white_D8,
+    marginBottom: 30,
+    textAlign: 'center',
+  },
 });
 
 export default onBoardStyle;
