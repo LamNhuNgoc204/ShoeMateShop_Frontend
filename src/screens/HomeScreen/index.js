14ad7626f6978e9d4bf995f32cf8a724c32448bd
Duc <cvducduy@gmail.com>
2024-12-11 07:59:18 +0700

    feat: fix update token

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,8 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {View, Image} from 'react-native';
-import {useTranslation} from 'react-i18next';
-import {useDispatch, useSelector} from 'react-redux';
-import {FlatList, ScrollView} from 'react-native-gesture-handler';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { View, Image } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import PagerView from 'react-native-pager-view';
 
 import homeStyle from './style';
@@ -12,20 +12,20 @@ import {
   fetchProductsThunk,
   fetchWishlist,
 } from '../../redux/thunks/productThunks';
-import {getCategoryThunk} from '../../redux/thunks/categoryThunk';
-import {BANNERS} from '../../api/mockData';
+import { getCategoryThunk } from '../../redux/thunks/categoryThunk';
+import { BANNERS } from '../../api/mockData';
 import Category from '../../items/Category';
 import HomeSkeleton from '../../placeholders/home';
-import {useFocusEffect} from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 import Loading from '../../components/Loading';
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AxiosInstance from '../../helpers/AxiosInstance';
 import ProductList from '../Product/ProductList';
-import {shuffleArray} from '../../utils/functions/formatData';
+import { shuffleArray } from '../../utils/functions/formatData';
 
-const HomeScreen = ({navigation, route}) => {
-  const {t} = useTranslation();
+const HomeScreen = ({ navigation, route }) => {
+  const { t } = useTranslation();
   const pagerRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(0);
   const [listProduct, setListProduct] = useState([]);
@@ -40,16 +40,14 @@ const HomeScreen = ({navigation, route}) => {
     console.log('upload token....');
     const fcmToken = await messaging().getToken();
     const preToken = await AsyncStorage.getItem('fcm_token');
-    if (preToken !== fcmToken) {
-      await AsyncStorage.setItem('fcm_token', fcmToken);
-      const response = await AxiosInstance().post('/users/refresh-fcm', {
-        token: fcmToken
-      })
-      if (response.status) {
-        console.log('refresh token thành công');
-      } else {
-        console.log('refresh token thất bại');
-      }
+    await AsyncStorage.setItem('fcm_token', fcmToken);
+    const response = await AxiosInstance().post('/users/refresh-fcm', {
+      token: fcmToken
+    })
+    if (response.status) {
+      console.log('refresh token thành công');
+    } else {
+      console.log('refresh token thất bại');
     }
   }
 
@@ -179,7 +177,7 @@ const HomeScreen = ({navigation, route}) => {
       {route?.params?.reload ? (
         <Loading />
       ) : (
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
           {loading ? (
             <HomeSkeleton />
           ) : (
@@ -201,7 +199,7 @@ const HomeScreen = ({navigation, route}) => {
                 </PagerView>
                 <FlatList
                   data={BANNERS}
-                  renderItem={({item, index}) => (
+                  renderItem={({ item, index }) => (
                     <View
                       style={[
                         homeStyle.indicatorDot,
@@ -224,7 +222,7 @@ const HomeScreen = ({navigation, route}) => {
                     scrollEnabled={false}
                     style={homeStyle.marginTop15}
                     data={categories}
-                    renderItem={({item, index}) => {
+                    renderItem={({ item, index }) => {
                       if (index % 2 === 0) {
                         return (
                           <Category
@@ -234,7 +232,7 @@ const HomeScreen = ({navigation, route}) => {
                             category={item}
                             style={[
                               index < categories.length - 2 &&
-                                homeStyle.marginRight30,
+                              homeStyle.marginRight30,
                               homeStyle.marginBottom15,
                             ]}
                           />
@@ -249,7 +247,7 @@ const HomeScreen = ({navigation, route}) => {
                   <FlatList
                     scrollEnabled={false}
                     data={categories}
-                    renderItem={({item, index}) => {
+                    renderItem={({ item, index }) => {
                       if (index % 2 === 1) {
                         return (
                           <Category
@@ -259,7 +257,7 @@ const HomeScreen = ({navigation, route}) => {
                             category={item}
                             style={[
                               index < categories.length - 2 &&
-                                homeStyle.marginRight30,
+                              homeStyle.marginRight30,
                             ]}
                           />
                         );
