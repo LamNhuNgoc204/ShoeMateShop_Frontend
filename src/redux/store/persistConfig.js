dbe437cd62fac6a8fada1ec66bd6f7da8d281634
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-12-17 02:23:11 +0700

    fix persist

--- src/redux/store/persistConfig.js
+++ src/redux/store/persistConfig.js
@@ -3,6 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  timeout: 10000,
 };
 
 export default persistConfig;
